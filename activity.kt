9fbd87c4130c632787ac94f256bcd95efddf0198
Samantha Chizengeya <61664915+Samantha123-lang@users.noreply.github.com>
2021-03-29 10:56:49 +0200

    Create activity.kt

---
+++ activity.kt
@@ -0,0 +1,12 @@
+package com.example.appassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication2.R
+
+class activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_questions)
+    }
+}
